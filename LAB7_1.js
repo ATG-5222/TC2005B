5613295025ad5c1d466ac3c4e73b3eb9c9057498
ATG-5222 <A01275222@tec.mx>
2022-02-23 00:41:03 -0600

    Ver como extraer valores de los campos.

--- LAB7_1.js
+++ LAB7_1.js
@@ -1,12 +1,20 @@
-let p1 = document.getElementById("psw_1").value;
-let p2 = document.getElementById("psw_2").value;
+let p1 = document.getElementById("psw1").value;
+let p2 = document.getElementById("psw2").value;
 
 alert("Procure no dejar ningun campo en blanco.");
+
+function verificar_campos(){
+    if (p1.length == 0 || p2.length == 0) {
+        alert("Los campos de la password no pueden quedar vacios");
+    }else {
+        alert("Campos llenos.");
+    }
+}
 
 function verificar_contraseña(){
     if (p1 == p2) {
         document.getElementById("verificar").innerHTML = "Las contraseñas coinciden, siga adelante.";
-      } else {
-        document.getElementById("verificar").innerHTML = "Las contraseñas no coinciden";
+    } else {
+        document.getElementById("verificar").innerHTML = "Las contraseñas no coinciden.";
     }
 }
