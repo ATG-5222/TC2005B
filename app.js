c6adce5aafb306d0e6587fccb26a9ffb8f5392f0
ATG-5222 <A01275222@tec.mx>
2022-02-24 12:40:44 -0600

    Ejecución de codigo asincrono.

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ console.log("Hola desde node");
 const filesystem = require('fs');
 
 filesystem.writeFileSync("hola.txt","Hola desde node");
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 0, 120, 2000, 340, 1000, 50];
+for(let item of arreglo){
+    setTimeout(()=>{
+        console.log(item);
+    },item);
+}
