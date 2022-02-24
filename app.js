decbaa2cc76ffd9c831bab7432cbc95126fc26d7
ATG-5222 <A01275222@tec.mx>
2022-02-24 13:05:22 -0600

    Primer servidor web con node.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 console.log("Hola desde node");
-
 //fs. filesystem
 const filesystem = require('fs');
-
 filesystem.writeFileSync("hola.txt","Hola desde node");
 
 const arreglo = [5000, 60, 90, 100, 10, 20, 0, 120, 2000, 340, 1000, 50];
@@ -11,3 +9,12 @@ for(let item of arreglo){
         console.log(item);
     },item);
 }
+
+//Ejecutando un servidor en node.js
+const http = require("http");
+const server = http.createServer((request,response) =>{
+    response.setHeader('Content-Type','text/html');
+    response.write('Hola');
+    response.end();
+});
+server.listen(3000);
