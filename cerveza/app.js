b43ad33ac64a1a4e948a7db5ee0e80b78fd65942
ATG-5222 <A01275222@tec.mx>
2022-03-01 12:40:46 -0600

    Codigos de clase.

---
+++ cerveza/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use('/capybaras', (request, response, next) => {
+    response.send('Respuesta de la ruta "/capybaras"'); 
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!,hola'); //Manda la respuesta
+});
+
+app.listen(3000);
