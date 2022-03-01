b4281e62dc3d25bc7dff2f6e55a130f7e3cfac7f
ATG-5222 <A01275222@tec.mx>
2022-03-01 13:00:33 -0600

    Rutas del server de Capybaras.

--- cerveza/app.js
+++ cerveza/app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const rutas_capybaras = require('./routes/capybaras.routes');
+
+app.use('/capybaras', rutas_capybaras);
+
 //Middleware
 app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
-app.use('/capybaras', (request, response, next) => {
-    response.send('Respuesta de la ruta "/capybaras"'); 
-});
-
 app.use((request, response, next) => {
     console.log('Otro middleware!');
-    response.send('¡Hola mundo!,hola'); //Manda la respuesta
+    response.send('¡Hola mundo!'); //Manda la respuesta
 });
 
 app.listen(3000);
