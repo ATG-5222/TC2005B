9adcdcfb165ec50281de56aa8dee44ce86bc9bb8
ATG-5222 <A01275222@tec.mx>
2022-03-02 00:17:19 -0600

    Adición de txt´s necesarios, probar /Blade&Soul en general.

--- LAB11/LAB11.js
+++ LAB11/LAB11.js
@@ -5,19 +5,12 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-const rutas_capybaras = require('\LAB11.routes\LAB11.routes.js');
-
-app.use('/capybaras', rutas_capybaras);
-
-//Middleware
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-});
+const ruta_bs = require('\LAB11.routes\LAB11_B&S.js');
+app.use('/Blade&Soul', ruta_bs);
 
 app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
+    respuesta = 'Content-Type', 'text/html<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>LAB10-ATG</title></head><body><h1 id="principal">Prueba del laboratorio 10: Rutas y formas.</h1><p>Aldo Tena García - A01275222</p><br><br><p>Bienvenido a la pagina principal.</p><p><strong>/TombRaider</strong> para la pagína de Tomb Raider.</p><p><strong>/Blade&Soul</strong> para la pagína de Blade & Soul.</p><p><strong>/CallofCthulhu</strong> para la pagína de Call of Cthulhu.</p><br><br><footer>Construcción de software y toma de decisiones.</footer></body>';
+    response.send(respuesta); 
 });
 
 app.listen(3000);
---
+++ LAB11/LAB11.routes/LAB11_B&S.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const router = express.Router();
+
+const fs = require("fs");
+const readline = require('readline');
+
+const clasesbs = [];
+const accounts = [];
+
+function leer_linea (txt,array){
+    const lectura = readline.createInterface({
+        input: fs.createReadStream(txt),
+        output: process.stdout,
+        console: false
+    });
+    lectura.on('line', function(linea) {
+        array.push(linea)
+    });
+}
+
+leer_linea('CLASES_B&S.txt',clasesbs);
+leer_linea('CUENTAS_B&S.txt',accounts);
+
+router.get('/nuevo', (request, response, next) => {
+    console.log('GET /Blade&Soul/nuevo');
+    let respuesta = '<!DOCTYPE html><html lang="es-mx"><head><title>Blade & Soul</title><meta charset="utf-8"</head><body><h1 id="principal">Blade & Soul</h1><h2>Comienza un viaje heroico para enfrentarte a las fuerzas oscuras y buscar venganza como un maestro de las artes marciales en Blade & Soul, el MMORPG gratuito.</h2><p>Clases presentes dentro del juego:</p><ul>';
+    for (let clase in clasesbs) {
+        respuesta +='<li>' + clasesbs[clase] + '</li>';
+    }
+    respuesta += 'Cuentas registradas:</p><ul>';
+    for (let cuenta in accounts) {
+        respuesta +='<li>' + accounts[cuenta] + '</li>';
+    }
+    respuesta += '<h2>Aquí nacen las nuevas leyendas:</h2><form action="Blade&Soul" method="POST"><label for="nombre">Ingrese el nuevo usuario: </label><input type="text" id="nombre" name="nombre" placeholder="Nombre de usuario"><br><br><input type="submit" value="Enviar"></form><br><br><a href="/">Regresar a la pagina principal</a></body>';
+    response.send(respuesta); 
+});
+
+router.post('/nuevo', (request, response, next) => {
+    console.log('POST /Blade&Soul/nuevo');
+    console.log(request.body);
+    fs.appendFile('CUENTAS_B&S.txt',"\n"+request.body.nombre, (err) => {
+        if (err) throw err;
+    });    
+    response.redirect('/Blade&Soul');
+});
+
+router.use('/', (request, response, next) => {
+    console.log('Ruta /Blade&Soul');
+    let respuesta = '<!DOCTYPE html><html lang="es-mx"><head><title>Blade & Soul</title><meta charset="utf-8"</head><body><h1 id="principal">Blade & Soul</h1><h2>Comienza un viaje heroico para enfrentarte a las fuerzas oscuras y buscar venganza como un maestro de las artes marciales en Blade & Soul, el MMORPG gratuito.</h2><p>Clases presentes dentro del juego:</p><ul>';
+    for (let clase in clasesbs) {
+        respuesta +='<li>' + clasesbs[clase] + '</li>';
+    }
+    respuesta += 'Cuentas registradas:</p><ul>';
+    for (let cuenta in accounts) {
+        respuesta +='<li>' + accounts[cuenta] + '</li>';
+    }
+    respuesta += '</ul><br></br><a href="/nuevo">Pantalla de creación de cuenta</a></body>';
+    response.send(respuesta); 
+});
+
+module.exports = router;
