f2b45d9f515a56ee91126dd33eadc0629352a473
ATG-5222 <A01275222@tec.mx>
2022-03-04 11:26:05 -0600

    Creación de views principales, faltas B&S.

--- LAB12/LAB12_HTML.js
+++ LAB12/LAB12_HTML.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const app = express();
-
+const path = requiere('path');
 const bodyParser = require('body-parser');
+const ruta_tb = require('./LAB11.routes/LAB11_TombRaider.js');
+const ruta_bs = require('./LAB11.routes/LAB11_B&S.js');
+const ruta_coc = require('./LAB11.routes/LAB11_CallofCthulhu.js');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views_lab12');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-const ruta_tb = require('./LAB11.routes/LAB11_TombRaider.js');
 app.use('/TombRaider', ruta_tb);
-const ruta_bs = require('./LAB11.routes/LAB11_B&S.js');
 app.use('/Blade&Soul', ruta_bs);
-const ruta_coc = require('./LAB11.routes/LAB11_CallofCthulhu.js');
 app.use('/CallofCthulhu', ruta_coc);
 
 app.use((request, response, next) => {
-    respuesta = '<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>LAB10-ATG</title></head><body><h1 id="principal">Prueba del laboratorio 10: Rutas y formas.</h1><p>Aldo Tena García - A01275222</p><br><br><p>Bienvenido a la pagina principal.</p><p><strong>/TombRaider</strong> para la pagína de Tomb Raider.</p><p><strong>/Blade&Soul</strong> para la pagína de Blade & Soul.</p><p><strong>/CallofCthulhu</strong> para la pagína de Call of Cthulhu.</p><br><br><footer>Construcción de software y toma de decisiones.</footer></body></html>';
-    response.send(respuesta); 
+    console.log("Prueba del lab 12");
 });
 
 app.use((request, response,next) => {
     console.log("Error 404")
-    response.statusCode = 404;
-    response.send("Error 404"); 
 });
 
 app.listen(3000);
