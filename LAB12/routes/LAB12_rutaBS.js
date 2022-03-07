d2a198f0ee73e23a73dbf3d344002ea2dc575d79
ATG-5222 <A01275222@tec.mx>
2022-03-07 13:26:01 -0600

    Avance lectura de archivos.

--- LAB12/routes/LAB12_rutaBS.js
+++ LAB12/routes/LAB12_rutaBS.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+var clasesbs = [];
+var accounts = [];
+
+fs.readFile('./INFO_BS/CLASES_BS.json', (err, data) => {
+    if (err) throw err;
+    clasesbs = JSON.parse(data);
+    console.log(clasesbs)
+});
+
+fs.readFile('./INFO_BS/CUENTAS_BS.json', (err, data) => {
+    if (err) throw err;
+    accounts = JSON.parse(data);
+    console.log(accounts)
+});
 
 const router = express.Router();
 
 router.use('/', (request, response, next) => {
     console.log('Use /Blade&Soul');
-    response.render('bs_principal');
+    response.render('bs_principal', {clasesbs: clasesbs, accounts:accounts})
 });
 
 module.exports = router;
