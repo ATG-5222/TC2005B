78790f7821dd4c7e98a3e17f5f783642232821f9
ATG-5222 <A01275222@tec.mx>
2022-03-23 00:15:00 -0600

    Ruta sign up.

--- LAB18/routes/LAB18_rutaUSER.js
+++ LAB18/routes/LAB18_rutaUSER.js
@@ -6,6 +6,8 @@ const user_controller = require('../controllers/USER_controller');
 router.get('/login', user_controller.get_login);
 router.post('/login', user_controller.login);
 router.get('/logout', user_controller.logout);
+router.get('/signup', user_controller.get_signup);
+router.post('/signup', user_controller.post_signup);
 router.get('/', user_controller.root);
 
 module.exports = router;
