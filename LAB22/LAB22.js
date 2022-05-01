ead77ce75e70c7b18f890ac3f1e8c3ea1534e5a9
ATG-5222 <A01275222@tec.mx>
2022-05-01 00:23:46 -0500

    Correcciones de Head y footer

--- LAB22/LAB22.js
+++ LAB22/LAB22.js
@@ -5,6 +5,19 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const csrf = require('csurf');
 const csrfProtection = csrf();
+const multer = require('multer');
+
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+    //'uploads': Es el directorio del servidor donde se subirán los archivos 
+    callback(null, 'uploads');
+},
+filename: (request, file, callback) => {
+//aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
+//para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+callback(null, new Date().getTime() + '-' + file.originalname);
+},
+});
 
 const app = express();
 app.use(cookieParser());
@@ -14,11 +27,13 @@ app.use(session({
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(csrfProtection); 
+app.use(multer({ storage: fileStorage }).single('imagen')); 
 
 app.use((request, response, next) => {
     response.locals.csrfToken = request.csrfToken();
